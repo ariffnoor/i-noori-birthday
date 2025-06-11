e52707fc5d9f11f37c84b1dee79bb079b95262a3
ariffnoor <arif17nouman@gmail.com>
2025-06-11 20:14:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,3 +16,21 @@ def letter():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/cake')
+def cake():
+    return render_template('cake.html')
+
+@app.route('/letter')
+def letter():
+    return render_template('letter.html')
+
+if __name__ == '__main__':
+    app.run()
